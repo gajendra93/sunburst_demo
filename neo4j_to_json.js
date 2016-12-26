e5abe1a5e19afbd5da44bf15443158f31d1ad204
gajendra93 <gajendrasp.rj.9693@gmail.com>
2016-12-26 18:07:56 +0530

    Created the json required
    Some manual data is there that we need to automate.

--- neo4j_to_json.js
+++ neo4j_to_json.js
@@ -1,24 +1,24 @@
 var cypher = require('cypher-stream')('bolt://localhost', 'neo4j', 'Gajsa9693#');
 var fs = require('fs');
 
+fs.writeFile('data.json', '');
 var data = {
 	"name": "Java Web Programming",
 	"children": []
-	};
-cypher('match (n:concept), (m:domain) return n,m limit 10')
+};
+// cypher('match (n:concept), (m:domain) return n,m limit 10')
+cypher('MATCH p=(n:concept)<-[:`subconcept of`]->(m) '+ 
+		'where NOT ()<-[:`subconcept of`]-(n) set m.size=1 WITH COLLECT(p) AS ps '+
+		'CALL apoc.convert.toTree(ps) yield value RETURN value')
 	.on('data', function (result){
-  		let obj = {};
-  		obj["name"] = result.n.name;
-  		obj["size"] = 1;
-  		data.children.push(obj);
-  		fs.writeFile('data.json', JSON.stringify(data), (err) => {
+  		data.children.push(result.value);
+	})
+	.on('end', function(d) {
+		fs.writeFile('data.json', JSON.stringify(data), (err) => {
 			if(err)
 				console.error(err);
 			else
 				console.log('File Created: data.json');
 		});
-	})
-	.on('end', function(data) {
-  		console.log('all done');
 	});
 
