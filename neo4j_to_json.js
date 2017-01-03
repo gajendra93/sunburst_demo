2dc725d53f307f41604e405453fd8889a9a9cdd4
asrinivas61 <adapasrinivas61@gmail.com>
2017-01-04 00:10:29 +0530

    updated sunburst ontology visualiser graph

--- neo4j_to_json.js
+++ neo4j_to_json.js
@@ -1,4 +1,4 @@
-var cypher = require('cypher-stream')('bolt://localhost', 'neo4j', 'Gajsa9693#');
+var cypher = require('cypher-stream')('bolt://localhost', 'neo4j', '123456');
 var fs = require('fs');
 
 fs.writeFile('data.json', '');
@@ -11,6 +11,12 @@ cypher('MATCH p=(n:concept)<-[:`subconcept of`]->(m) '+
 		'where NOT ()<-[:`subconcept of`]-(n) set m.size=1 WITH COLLECT(p) AS ps '+
 		'CALL apoc.convert.toTree(ps) yield value RETURN value')
 	.on('data', function (result){
+		// if(result.value['subconcept of'].length>0){
+		// 	result.value['subconcept of'].forEach(function(item){
+		// 		item['color'] = Math.floor(Math.random() * (5 - 0 + 1) + 0);
+		// 	})
+		// }
+		// result.value["color"] = Math.floor(Math.random() * (5 - 0 + 1) + 0);
   		data.children.push(result.value);
 	})
 	.on('end', function(d) {
