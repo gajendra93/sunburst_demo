577738657def35fba40e6c3c371f9f72d57e4dd5
charan <charansai.iitg@gmail.com>
2017-01-04 15:15:16 +0530

    Updated multi-level heirarchy sunburst

--- neo4j_to_json.js
+++ neo4j_to_json.js
@@ -1,30 +1,44 @@
-var cypher = require('cypher-stream')('bolt://localhost', 'neo4j', '123456');
+var cypher = require('cypher-stream')('bolt://localhost', 'neo4j', 'charan');
 var fs = require('fs');
 
-fs.writeFile('data.json', '');
-var data = {
-	"name": "Java Web Programming",
-	"children": []
+var result1=[];
+var tree = {
+    "name": "Java Web Programming",
+    "children": []
 };
-// cypher('match (n:concept), (m:domain) return n,m limit 10')
-cypher('MATCH p=(n:concept)<-[:`subconcept of`]->(m) '+ 
-		'where NOT ()<-[:`subconcept of`]-(n) set m.size=1 WITH COLLECT(p) AS ps '+
-		'CALL apoc.convert.toTree(ps) yield value RETURN value')
-	.on('data', function (result){
-		// if(result.value['subconcept of'].length>0){
-		// 	result.value['subconcept of'].forEach(function(item){
-		// 		item['color'] = Math.floor(Math.random() * (5 - 0 + 1) + 0);
-		// 	})
-		// }
-		// result.value["color"] = Math.floor(Math.random() * (5 - 0 + 1) + 0);
-  		data.children.push(result.value);
-	})
-	.on('end', function(d) {
-		fs.writeFile('data.json', JSON.stringify(data), (err) => {
-			if(err)
-				console.error(err);
-			else
-				console.log('File Created: data.json');
-		});
-	});
+
+cypher('match (n:Concept) return n.context AS context,n.name AS name,n.conceptid AS conceptid,n.desc as desc,n.parent AS parent')
+    .on('data', function (result){
+      result1.push(result);
+    })
+    .on('end', function() {
+        console.log('all done');
+        var dataMap = result1.reduce(function(map, node) {
+            map[node.conceptid] = node;
+            return map;
+        }, {});
+        result1.forEach(function(node) {
+            var parent = dataMap[node.parent];
+            node.size=1;
+            if (parent) {
+                (parent.children || (parent.children = []))
+                    .push(node);
+            } 
+            else {
+                tree.children.push(node);
+            }
+        });
+    })
+    .on('end', function() {
+        var p3=JSON.stringify(tree);
+        p3=p3.replace("[","[\n\t");
+        p3= p3.replace(/},/g,"},\n\t");
+        p3= p3.replace(/\\"/g,"");
+        p3=p3.replace(/,/g,",\n\t");
+        fs.writeFile("data.json",p3,function(err) {
+            if(err){
+                throw err;
+            }
+        });
+    })
 
